8b8588c65fe64f357ad92035c1370a90c89314a0
Aschale Siyoum <aschaled@yahoo.com>
2013-12-02 17:40:13 -0500

    eloquent-javascript

---
+++ ch6.js
@@ -0,0 +1,254 @@
+function countZeroes(array) {
+  function counter(total, element) {
+    return total + (element === 0 ? 1 : 0);
+  }
+  return reduce(counter, 0, array);
+}
+
+function count(test, array) {
+  return reduce(function(total, element) {
+    return total + (test(element) ? 1 : 0);
+  }, 0, array);
+}
+
+function equals(x) {
+  return function(element) {return x === element;};
+}
+
+function countZeroes(array) {
+  return count(equals(0), array);
+}
+
+
+var paragraphs = recluseFile().split("\n\n");
+print("Found ", paragraphs.length, " paragraphs.");
+
+
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
+
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: paragraph};
+}
+
+show(processParagraph(paragraphs[0]));
+
+
+
+var paragraphs = map(processParagraph,
+                     recluseFile().split("\n\n"));
+
+
+
+function splitParagraph(text) {
+  function indexOrEnd(character) {
+    var index = text.indexOf(character);
+    return index == -1 ? text.length : index;
+  }
+
+  function takeNormal() {
+    var end = reduce(Math.min, text.length,
+                     map(indexOrEnd, ["*", "{"]));
+    var part = text.slice(0, end);
+    text = text.slice(end);
+    return part;
+  }
+
+  function takeUpTo(character) {
+    var end = text.indexOf(character, 1);
+    if (end == -1)
+      throw new Error("Missing closing '" + character + "'");
+    var part = text.slice(1, end);
+    text = text.slice(end + 1);
+    return part;
+  }
+
+  var fragments = [];
+
+  while (text != "") {
+    if (text.charAt(0) == "*")
+      fragments.push({type: "emphasised",
+                      content: takeUpTo("*")});
+    else if (text.charAt(0) == "{")
+      fragments.push({type: "footnote",
+                      content: takeUpTo("}")});
+    else
+      fragments.push({type: "normal",
+                      content: takeNormal()});
+  }
+  return fragments;
+}
+
+
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
+
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: splitParagraph(paragraph)};
+}
+
+
+function extractFootnotes(paragraphs) {
+  var footnotes = [];
+  var currentNote = 0;
+
+  function replaceFootnote(fragment) {
+    if (fragment.type == "footnote") {
+      currentNote++;
+      footnotes.push(fragment);
+      fragment.number = currentNote;
+      return {type: "reference", number: currentNote};
+    }
+    else {
+      return fragment;
+    }
+  }
+
+  forEach(paragraphs, function(paragraph) {
+    paragraph.content = map(replaceFootnote,
+                            paragraph.content);
+  });
+
+  return footnotes;
+} 
+
+
+var linkObject = {name: "a",
+                  attributes: {href: "http://www.gokgs.com/"},
+                  content: ["Play Go!"]};
+                  
+                  
+function tag(name, content, attributes) {
+  return {name: name, attributes: attributes, content: content};
+}
+
+function link(target, text) {
+  return tag("a", [text], {href: target});
+}
+
+function htmlDoc(title, bodyContent) {
+  return tag("html", [tag("head", [tag("title", [title])]),
+                      tag("body", bodyContent)]);
+}
+
+
+function image(src) {
+  return tag("img", [], {src: src});
+}
+
+function escapeHTML(text) {
+  var replacements = [[/&/g, "&amp;"], [/"/g, "&quot;"],
+                      [/</g, "&lt;"], [/>/g, "&gt;"]];
+  forEach(replacements, function(replace) {
+    text = text.replace(replace[0], replace[1]);
+  });
+  return text;
+}
+
+
+function renderHTML(element) {
+  var pieces = [];
+
+  function renderAttributes(attributes) {
+    var result = [];
+    if (attributes) {
+      for (var name in attributes) 
+        result.push(" " + name + "=\"" +
+                    escapeHTML(attributes[name]) + "\"");
+    }
+    return result.join("");
+  }
+
+  function render(element) {
+    // Text node
+    if (typeof element == "string") {
+      pieces.push(escapeHTML(element));
+    }
+    // Empty tag
+    else if (!element.content || element.content.length == 0) {
+      pieces.push("<" + element.name +
+                  renderAttributes(element.attributes) + "/>");
+    }
+    // Tag with content
+    else {
+      pieces.push("<" + element.name +
+                  renderAttributes(element.attributes) + ">");
+      forEach(element.content, render);
+      pieces.push("</" + element.name + ">");
+    }
+  }
+
+  render(element);
+  return pieces.join("");
+}
+
+
+function renderParagraph(paragraph) {
+  return tag(paragraph.type, map(renderFragment,
+                                 paragraph.content));
+}
+
+function renderFragment(fragment) {
+  if (fragment.type == "reference")
+    return footnote(fragment.number);
+  else if (fragment.type == "emphasised")
+    return tag("em", [fragment.content]);
+  else if (fragment.type == "normal")
+    return fragment.content;
+}
+
+function renderFootnote(footnote) {
+  var number = "[" + footnote.number + "] ";
+  var anchor = tag("a", [number], {name: "footnote" + footnote.number});
+  return tag("p", [tag("small", [anchor, footnote.content])]);
+}
+
+
+function renderFile(file, title) {
+  var paragraphs = map(processParagraph, file.split("\n\n"));
+  var footnotes = map(renderFootnote,
+                      extractFootnotes(paragraphs));
+  var body = map(renderParagraph, paragraphs).concat(footnotes);
+  return renderHTML(htmlDoc(title, body));
+}
+
+viewHTML(renderFile(recluseFile(), "The Book of Programming"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
